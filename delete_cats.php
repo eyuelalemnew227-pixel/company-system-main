7e6b73ccc000735435a72f118a709ccd1aee93fc
Eyuel <eyuelalemnew227@gmail.com>
2026-07-28 14:10:30 +0300

    Cleanup: Sweep of obsolete debug scripts from root workspace

--- delete_cats.php
+++
@@ -1,8 +0,0 @@
-<?php
-require __DIR__ . '/vendor/autoload.php';
-$app = require_once __DIR__ . '/bootstrap/app.php';
-$kernel = $app->make(Illuminate\Contracts\Console\Kernel::class);
-$kernel->bootstrap();
-
-App\Models\EvaluationCategory::whereIn('id', [1, 7])->delete();
-echo "Deleted dummy categories.\n";
