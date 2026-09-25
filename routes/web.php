80a07c75999587dd07e52e6cf60ef5a96477749c
tsina-tefe <tsinukaltefera34@gmail.com>
2026-09-25 10:34:19 +0300

    Fix form permissions POST route and update package-lock.json

--- routes/web.php
+++ routes/web.php
@@ -174,6 +174,7 @@ Route::middleware(['auth', 'verified'])->group(function () {
         Route::resource('forms', FormController::class)->except(['destroy']);
         Route::get('/forms/{form}/versions', [FormController::class, 'versions'])->name('forms.versions');
         Route::get('/forms/{form}/permissions', [\App\Http\Controllers\FormPermissionController::class, 'index'])->name('forms.permissions');
+        Route::post('/forms/{form}/permissions', [\App\Http\Controllers\FormPermissionController::class, 'update'])->name('forms.permissions.update');
     });
 
     // KPI Library Management
